15b58fea2e5c6a1cf559833a7df5489378a4d024
Nate <nathanw88@gmail.com>
2019-01-09 17:31:09 -0600

    minor changes trying to fix heroku

--- models/index.js
+++ models/index.js
@@ -9,15 +9,13 @@ var config = require(__dirname + '/../config/config.json')[env];
 var db = {};
 
 var sequelize;
-if (process.env.JAWSDB_URL) {
-  sequelize = mysql.createConnection(process.env.JAWSDB_URL);
-  } else {
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-  }
+  
 
 fs
   .readdirSync(__dirname)
